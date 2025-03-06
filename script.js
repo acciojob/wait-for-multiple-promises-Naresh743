ac350b532311b5ba567e49b66145aa71d8d93e00
Naresh Devra <nareshdevra743@gmail.com>
2025-03-06 10:47:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function getRandomTime() {
+            return Math.random() * (3 - 1) + 1; // Random time between 1 and 3 seconds
+        }
+
+        const promise1 = new Promise(resolve => setTimeout(() => resolve(getRandomTime()), getRandomTime() * 1000));
+        const promise2 = new Promise(resolve => setTimeout(() => resolve(getRandomTime()), getRandomTime() * 1000));
+        const promise3 = new Promise(resolve => setTimeout(() => resolve(getRandomTime()), getRandomTime() * 1000));
+
+        const startTime = performance.now();
+
+        Promise.all([promise1, promise2, promise3]).then(times => {
+            const totalTime = (performance.now() - startTime) / 1000;
+            const output = document.getElementById('output');
+            output.innerHTML = '';
+
+            times.forEach((time, index) => {
+                const row = `<tr><td>Promise ${index + 1}</td><td>${time.toFixed(3)}</td></tr>`;
+                output.innerHTML += row;
+            });
+
+            output.innerHTML += `<tr><td>Total</td><td>${totalTime.toFixed(3)}</td></tr>`;
+        });
